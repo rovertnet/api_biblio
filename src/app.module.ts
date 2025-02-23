74c72487c523019f6e7918e49255445346afb874
Robert Matundu <matundukabamba@gmail.com>
2025-02-24 00:35:09 +0100

    another commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,18 +5,24 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'biblio',
-      entities: [],
-      synchronize: true,
-    }),
+    configModule.forRoot()
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    });
   ],
   controllers: [AppController],
   providers: [AppService],
-})
+}),
 export class AppModule {}
