e1b223b02c2aace29ff008cc10171df7a3f6c9a8
Robert Matundu <matundukabamba@gmail.com>
2025-03-02 22:09:05 +0100

    last commit

---
+++ src/todos/todo.service.ts
@@ -0,0 +1,11 @@
+import { InjectRepository } from "@nestjs/typeorm";
+import { Todo } from "./todo.entity";
+import { Repository } from "typeorm";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class TodoService {
+  constructor(@InjectRepository(Todo) private readonly TodoRepository: Repository<Todo>,){}
+
+  create(dto: {titre: string})
+}
