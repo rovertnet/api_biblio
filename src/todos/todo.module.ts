3a7efc6623c8c17e804b06f283ccb8dce408170b
Robert Matundu <matundukabamba@gmail.com>
2025-03-03 17:46:31 +0100

    last commit

--- src/todos/todo.module.ts
+++ src/todos/todo.module.ts
@@ -1,9 +1,13 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from "@nestjs/common";
 import { Todo } from './todo.entity';
+import { TodoController } from './todo.controller';
+import { TodoService } from './todo.service';
 
 
 @Module({
   imports: [TypeOrmModule.forFeature([Todo])],
+  controllers: [TodoController],
+  providers: [TodoService],
 })
 export class TodoMudule {}
